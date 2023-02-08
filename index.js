7efb93885dd5f5ec5384139f586277c564fa47ee
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2023-02-07 17:41:18 -0800

    build(projects): fixed JS to add toggle to each project title section

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 'use strict';
 
+
+// const arrow = document.querySelector('.project-info1');
+
+
+const projectTitles = document.querySelectorAll('.project-info');
+
+const hiddenSections = document.querySelectorAll('.hidden-proj-info');
+
+for(let i = 0; i < projectTitles.length; i++) {
+    const projClassList = hiddenSections[i].classList;
+
+    projectTitles[i].addEventListener('click', function () {
+        if (projClassList.contains('hidden')) {
+            projClassList.remove('hidden')
+        } else {
+            projClassList.add('hidden');
+        }
+    });
+}
+
+// arrow.addEventListener('click', function () {
+//     if (projClassList.contains('hidden')) {
+//         projClassList.remove('hidden')
+//     } else {
+//         projClassList.add('hidden');
+//     }
+// });
