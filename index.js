a054296c60b577328650360f5f861bda26d24c1e
mkurmarc <marc.rios1712@gmail.com>
2023-05-28 14:52:57 -0700

    build: about.html

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-'use strict';
-
+"use strict";
 
 // const arrow = document.querySelector('.project-info1');
 
+const projectTitles = document.querySelectorAll(".project-info");
 
-const projectTitles = document.querySelectorAll('.project-info');
+const hiddenSections = document.querySelectorAll(".hidden-proj-info");
 
-const hiddenSections = document.querySelectorAll('.hidden-proj-info');
+for (let i = 0; i < projectTitles.length; i++) {
+  const projClassList = hiddenSections[i].classList;
 
-for(let i = 0; i < projectTitles.length; i++) {
-    const projClassList = hiddenSections[i].classList;
-
-    projectTitles[i].addEventListener('click', function () {
-        if (projClassList.contains('hidden')) {
-            projClassList.remove('hidden')
-        } else {
-            projClassList.add('hidden');
-        }
-    });
+  projectTitles[i].addEventListener("click", function () {
+    if (projClassList.contains("hidden")) {
+      projClassList.remove("hidden");
+    } else {
+      projClassList.add("hidden");
+    }
+  });
 }
 
 // arrow.addEventListener('click', function () {
