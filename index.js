5afa636c2ec9704cc988ac9f00313a2dc3554cb7
mkurmarc <marc.rios1712@gmail.com>
2023-05-28 17:02:24 -0700

    refactor: about page is in about directory as index.html linked styling to about page chaged some of the about me content

--- index.js
+++ index.js
@@ -18,10 +18,4 @@ for (let i = 0; i < projectTitles.length; i++) {
   });
 }
 
-// arrow.addEventListener('click', function () {
-//     if (projClassList.contains('hidden')) {
-//         projClassList.remove('hidden')
-//     } else {
-//         projClassList.add('hidden');
-//     }
-// });
+// return the background color used on this website https://eamonnlavelle.com/about.html    #f5f5f5
